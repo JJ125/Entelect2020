119ddd198ffeb2ddb3742103991f79c3c0613952
JJ Jordaan <jordaan125@gmail.com>
2020-04-09 22:37:59 +0200

    First commit of base code.

---
+++ reference-bot/java/src/main/java/za/co/entelect/challenge/Bot.java
@@ -0,0 +1,64 @@
+package za.co.entelect.challenge;
+
+import za.co.entelect.challenge.command.*;
+import za.co.entelect.challenge.entities.*;
+import za.co.entelect.challenge.enums.Terrain;
+
+import java.util.*;
+
+import static java.lang.Math.max;
+
+import java.security.SecureRandom;
+
+public class Bot {
+
+    private static final int maxSpeed = 9;
+    private List<Command> directionList = new ArrayList<>();
+
+    private final Random random;
+
+    private final static Command ACCELERATE = new AccelerateCommand();
+
+    private final static Command TURN_RIGHT = new ChangeLaneCommand(1);
+    private final static Command TURN_LEFT = new ChangeLaneCommand(-1);
+
+    public Bot() {
+        this.random = new SecureRandom();
+        directionList.add(TURN_LEFT);
+        directionList.add(TURN_RIGHT);
+    }
+
+    public Command run(GameState gameState) {
+        Car myCar = gameState.player;
+        Car opponent = gameState.opponent;
+
+        List<Object> blocks = getBlocksInFront(myCar.position.lane, myCar.position.block, gameState);
+        if (blocks.contains(Terrain.MUD)) {
+            int i = random.nextInt(directionList.size());
+            return directionList.get(i);
+        }
+        return ACCELERATE;
+    }
+
+    /**
+     * Returns map of blocks and the objects in the for the current lanes, returns
+     * the amount of blocks that can be traversed at max speed.
+     **/
+    private List<Object> getBlocksInFront(int lane, int block, GameState gameState) {
+        List<Lane[]> map = gameState.lanes;
+        List<Object> blocks = new ArrayList<>();
+        int startBlock = map.get(0)[0].position.block;
+
+        Lane[] laneList = map.get(lane - 1);
+        for (int i = max(block - startBlock, 0); i <= block - startBlock + Bot.maxSpeed; i++) {
+            if (laneList[i] == null || laneList[i].terrain == Terrain.FINISH) {
+                break;
+            }
+
+            blocks.add(laneList[i].terrain);
+
+        }
+        return blocks;
+    }
+
+}
